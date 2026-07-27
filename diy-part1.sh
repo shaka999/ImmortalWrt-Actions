f35d0ccd9dec587e5f575dd5aed322f028fc7369
WorkBuddy <builder@local>
2026-07-27 15:22:50 +0800

    x86: 移除 homeproxy/nikki/momo 代理栈, 新增 OpenClash (含 zh-cn), 加 openclash feed

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,4 @@
 # Add a feed source
 echo 'src-git immortalwrt https://github.com/whiskyrye/ImmortalWrt-Packages.git;main' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >>feeds.conf.default
 
